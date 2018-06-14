94d4806f95b7e616d5e76d1a47e4187b3932195f
wuzhigang <1402832352@qq.com>
2018-06-14 08:45:45 +0800

    Initial Commit

---
+++ SENSINGM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SENSINGM
+//
+//  Created by 吴志刚 on 2018/6/14.
+//  Copyright © 2018 吴志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
